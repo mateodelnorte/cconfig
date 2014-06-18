604535d13c1a31caebdcfd0d0512d95fb1f9d995
Narciso Guillen <narciso.guillen@onelogin.com>
2014-06-18 18:13:31 -0500

    Make sure Enviroments don't break / no need to re set configPath

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var extend = require('extend');
-var log = require('debug')('config');
-var path = require('path');
+var log    = require('debug')('config');
+var path   = require('path');
 
-var configEnvironments = process.env.CONFIG_ENVIRONMENTS
-													? process.env.CONFIG_ENVIRONMENTS.split(',') 
-													: ['development', 'qa', 'production'];
+var configEnvironments = Array.isArray(process.env.CONFIG_ENVIRONMENTS)
+												 ? process.env.CONFIG_ENVIRONMENTS.split(',')
+												 : ['development', 'qa', 'production'];
 
-log('initializing config with environments %j', configEnvironments);													
+log('initializing config with environments %j', configEnvironments);
 
-module.exports = function (configPath) {
-	configPath = configPath || process.env.CONFIG_PATH || path.join(process.cwd(), 'config.json');
+module.exports = function(configPath){
+
+  (configPath) || (configPath = process.env.CONFIG_PATH || path.join(process.cwd(), 'config.json'));
+
 	var config = require(configPath);
-	var env = process.env.NODE_ENV || 'development';
-	var tmp = config[env];
+	var env    = process.env.NODE_ENV || 'development';
+	var tmp    = config[env];
+
 	configEnvironments.forEach(function (environment) {
 		delete config[environment];
 	});
-	extend(config, tmp, process.env);
-	return config;
-}
+
+	return extend(config, tmp, process.env);
+
+};
