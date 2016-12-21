8c862ceae7522f8aa954a9bc87fc9ee7b5fe1d88
Patrick Scott <patrick.scott@zx-ventures.com>
2016-12-21 18:01:52 -0500

    allow developers to pass an object instead of a path to cconfig

--- index.js
+++ index.js
@@ -15,30 +15,36 @@ var config = null;
 module.exports = function (configPath) {
 
 	if (config) return config;
-	// First, get the configPath - this is either passed in when module is required
-	// set in an environment variable named CONFIG_PATH, or found in config.json
-	configPath = configPath || process.env.CONFIG_PATH || path.join(process.cwd(), 'config.json');
 
-	// Build the config from the configPath
-	
-	try { // config.json
-		config = require(configPath);
-		log(`found json config file at ${configPath}`);
-	} catch (e) {
-		log(`no json config file found at ${configPath}`);
-		// no file found at config.json
-		configPath = path.join(process.cwd(), 'config.js');
+	if (typeof configPath === 'object') {
+		config = configPath;
+	} else {
 
-		try { // config.js
+		// First, get the configPath - this is either passed in when module is required
+		// set in an environment variable named CONFIG_PATH, or found in config.json
+		configPath = configPath || process.env.CONFIG_PATH || path.join(process.cwd(), 'config.json');
+
+		// Build the config from the configPath
+		
+		try { // config.json
 			config = require(configPath);
-			log(`found js config file at ${configPath}`);
+			log(`found json config file at ${configPath}`);
 		} catch (e) {
-			log(`no js config file found at ${configPath}`);
+			log(`no json config file found at ${configPath}`);
 			// no file found at config.json
-			throw new Error('could not find valid config.json or config.js file');
-		}	
+			configPath = path.join(process.cwd(), 'config.js');
 
-	}
+			try { // config.js
+				config = require(configPath);
+				log(`found js config file at ${configPath}`);
+			} catch (e) {
+				log(`no js config file found at ${configPath}`);
+				// no file found at config.json
+				throw new Error('could not find valid config.json or config.js file');
+			}	
+
+		}
+	} 
 
 	const env = process.env.NODE_ENV || 'development';
 	const currentEnvConfig = config[env];
